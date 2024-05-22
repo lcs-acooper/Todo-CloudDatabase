66053db27b95fc7545f9868e0f9381422ddfc69a
AJ Cooper <acooper@lcs.on.ca>
2024-05-22 12:18:46 -0400

    Adjusted view model to pull to-do items from the database hosted at Supabase.

--- TodoList/Helpers/Supabase.swift
+++ TodoList/Helpers/Supabase.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://arbfvcxeprliopjmpwod.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyYmZ2Y3hlcHJsaW9wam1wd29kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM2NTE5OTYsImV4cCI6MjAyOTIyNzk5Nn0.MU_y1vvVUK_zx2gh3QrsqHuhfpht_jzm_kVmrVB0J8E"
+)
