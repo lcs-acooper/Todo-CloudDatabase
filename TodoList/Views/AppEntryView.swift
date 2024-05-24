682031bc3ab99432595b319735722ed968b5a5ec
AJ Cooper <acooper@lcs.on.ca>
2024-05-23 20:05:17 -0400

    Added an authentication flow so that users are signed-in anonymously and assigned a unique user id. This prevents one user from seeing another user's data.

---
+++ TodoList/Views/AppEntryView.swift
@@ -0,0 +1,8 @@
+//
+//  AppEntryView.swift
+//  TodoList
+//
+//  Created by  Adam-James  Cooper on 2024-05-23.
+//
+
+import Foundation
---
+++ TodoList/Views/AuthView.swift
@@ -0,0 +1,8 @@
+//
+//  AuthView.swift
+//  TodoList
+//
+//  Created by  Adam-James  Cooper on 2024-05-23.
+//
+
+import Foundation
