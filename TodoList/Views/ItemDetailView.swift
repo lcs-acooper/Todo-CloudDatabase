96d92bb2833d59b0ac1e5d694ec9b990562a7c57
AJ Cooper <acooper@lcs.on.ca>
2024-06-01 12:37:09 -0400

    Added a detail view for to-do items that have an image.

---
+++ TodoList/Views/ItemDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetailView.swift
+//  TodoList
+//
+//  Created by  Adam-James  Cooper on 2024-06-01.
+//
+
+import SwiftUI
+
+struct ItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetailView()
+}
